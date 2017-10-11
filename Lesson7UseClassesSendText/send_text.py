c6be6bf15386df8a9766c14dd3b0ac8ce9960242
Kennedy DeSousa <kennedydesousa@gmail.com>
2017-10-11 13:28:15 -0700

    Added correction to text document

--- Lesson7UseClassesSendText/send_text.py
+++ Lesson7UseClassesSendText/send_text.py
@@ -3,7 +3,7 @@ from twilio.rest import Client
 # Your Account SID from twilio.com/console
 account_sid = "AC579b349414e2ab43c186ca84b9a6403b"
 # Your Auth Token from twilio.com/console
-auth_token  = "f85ea251a7a877325e73d6719fc57660"
+auth_token  = "authcode"
 
 client = Client(account_sid, auth_token)
 
