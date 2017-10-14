2ef5d6a51fa71cf775095dff143c3cf5b4b382c0
Kennedy DeSousa <kennedydesousa@gmail.com>
2017-10-14 15:20:28 -0700

    quiz create new instance of favorite movie

--- Lesson9MakeClassesMovieWebsite/movies/entertainment_center.py
+++ Lesson9MakeClassesMovieWebsite/movies/entertainment_center.py
@@ -1,8 +1,19 @@
 import media 
 
-toy_story = media.Movie("Toy Story","A story about a boy and his toys that come to life","https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg","https://youtu.be/Ex2bidLrVWE")
+toy_story = media.Movie("Toy Story",
+                        "A story about a boy and his toys that come to life",
+                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "https://youtu.be/tN1A2mVnrOM")
+avatar = media.Movie("Avatar",
+                     "A marine falls goes to another planet",
+                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://youtu.be/5PSNL1qE6VY")
+demons = media.Movie("Demons",
+                     "young friends are invited to a movie theater only to be trapped",
+                     "https://upload.wikimedia.org/wikipedia/en/2/2b/Demonsposter1986.jpg",
+                     "https://youtu.be/PwX0hOpsuec"
+                     )
 
 
-print(toy_story.storyline)
-print(toy_story.title)
-
+print(avatar.title)
+demons.show_trailer()
--- Lesson9MakeClassesMovieWebsite/movies/media.py
+++ Lesson9MakeClassesMovieWebsite/movies/media.py
@@ -1,7 +1,11 @@
+import webbrowser
 
 class Movie():
-    def __init__(self,movie_title,movie_storyline,movie_poster_url,movie_trailer_url):
+    def __init__(self,movie_title,movie_storyline,movie_poster_url,trailer_youtube):
         self.title = movie_title
-        storyline = movie_storyline
+        self.storyline = movie_storyline
         self.poster_url = movie_poster_url
-        self.trailer_url = movie_trailer_url
+        self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
