7c4f4def036f4bbb30583f01c5678a1e5e89d06a
Kennedy DeSousa <kennedydesousa@gmail.com>
2017-10-14 14:34:28 -0700

    media lesson progress. Not completed

---
+++ Lesson9MakeClassesMovieWebsite/movies/entertainment_center.py
@@ -0,0 +1,8 @@
+import media 
+
+toy_story = media.Movie("Toy Story","A story about a boy and his toys that come to life","https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg","https://youtu.be/Ex2bidLrVWE")
+
+
+print(toy_story.storyline)
+print(toy_story.title)
+
---
+++ Lesson9MakeClassesMovieWebsite/movies/media.py
@@ -0,0 +1,7 @@
+
+class Movie():
+    def __init__(self,movie_title,movie_storyline,movie_poster_url,movie_trailer_url):
+        self.title = movie_title
+        storyline = movie_storyline
+        self.poster_url = movie_poster_url
+        self.trailer_url = movie_trailer_url
