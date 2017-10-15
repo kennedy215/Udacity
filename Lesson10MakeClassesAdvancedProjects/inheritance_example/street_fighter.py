33b81d3d09ef5d9ad5e7254b6f275db85988144c
Kennedy DeSousa <kennedydesousa@gmail.com>
2017-10-15 10:36:28 -0700

    example of inheritance

---
+++ Lesson10MakeClassesAdvancedProjects/inheritance_example/street_fighter.py
@@ -0,0 +1,9 @@
+class Parent():
+    def __init__(self,last_name,eye_color):
+        #print("Parent Constructor Called")
+        self.last_name = last_name
+        self.eye_color = eye_color
+
+#test normall this would be in a seperate file... Example is not on a seperate
+#file
+
---
+++ Lesson10MakeClassesAdvancedProjects/inheritance_example/street_fighter_attributes.py
@@ -0,0 +1,10 @@
+import street_fighter
+
+ken_masters = street_fighter.Parent("Masters",
+                                    "Brown")
+chung_li = street_fighter.Parent("Li",
+                                 "Brown")
+
+print(ken_masters.last_name) ,( ken_masters.eye_color) 
+print(chung_li.last_name) ,( chung_li.eye_color),
+
