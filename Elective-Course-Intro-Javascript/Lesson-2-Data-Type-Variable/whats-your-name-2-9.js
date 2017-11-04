c67df4ac00b219a72668f33c8b8db5e32c555ae1
Kennedy DeSousa <kennedydesousa@gmail.com>
2017-11-03 23:25:42 -0700

    name variable

---
+++ Elective-Course-Intro-Javascript/Lesson-2-Data-Type-Variable/whats-your-name-2-9.js
@@ -0,0 +1,8 @@
+// Directions:
+// Create a variable called fullName and assign it your full name as a string.
+//
+/*
+ * Programming Quiz: What's my Name? (2-9)
+ */
+var fullName = "Kennedy DeSousa";
+console.log(fullName);
