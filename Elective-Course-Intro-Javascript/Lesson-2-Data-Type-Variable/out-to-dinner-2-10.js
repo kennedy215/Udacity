923bec9708362f1df156615b8f029fafb7dd0956
Kennedy DeSousa <kennedydesousa@gmail.com>
2017-11-03 23:29:43 -0700

    dinner

---
+++ Elective-Course-Intro-Javascript/Lesson-2-Data-Type-Variable/out-to-dinner-2-10.js
@@ -0,0 +1,16 @@
+// Print the total to the JavaScript console.
+//
+// Hint: 15% in decimal form is written as 0.15.
+//
+// TIP: To print out the total with a dollar sign ( $ ) use string concatenation. To round total up by two decimal points use the toFixed() method. To use toFixed() pass it the number of decimal points you want to use. For example, if total equals 3.9860, then total.toFixed(2) would return 3.99.
+
+/*
+ * Programming Quiz: Out to Dinner (2-10)
+ */
+var bill = 10.25 + 3.99 + 7.15;
+var tip = bill * .15;
+var total = bill + tip;
+
+console.log(total);
+
+// your code goes here
