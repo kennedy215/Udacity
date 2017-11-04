6893a98cd320b6b33d06cc4e05e896025dfd20fa
Kennedy DeSousa <kennedydesousa@gmail.com>
2017-11-03 23:15:19 -0700

    semi colons and haiku

---
+++ Elective-Course-Intro-Javascript/Lesson-2-Data-Type-Variable/semi-colons-2-8.js
@@ -0,0 +1,15 @@
+// Directions:
+// Define two variables called thingOne and thingTwo and assign them values. Print the values of both variables in one console.log statement using concatenation. For example,
+//
+// red blue
+// where "red" is the value of thingOne and "blue" is the value of thingTwo. Don't forget to use semicolons!
+//
+// Your Code:
+/*
+ * Programming Quiz: Semicolons! (2-8)
+ */
+ var thingOne = "red";
+ var thingTwo = "blue";
+ console.log(thingOne+thingTwo);
+
+// your code goes here
---
+++ Elective-Course-Intro-Javascript/Lesson-2-Data-Type-Variable/yosin-bosin-2-6.js
@@ -0,0 +1,18 @@
+// Directions:
+// Build a string using concatenation by combining the lines from this famous haiku poem by Yosa Buson.
+//
+// Blowing from the west
+// Fallen leaves gather
+// In the east.
+// Each string should be printed on its own line. Here is a link to our lesson on Escaped Characters as a reference.
+//
+// Hint: You will need to use special characters to produce the following output.
+//
+// Your Code:
+
+/*
+ * Programming Quiz: Yosa Buson (2-6)
+ */
+
+var haiku = "Blowing from the west"+"\nFallen leaves gather"+"\nIn the east.";
+console.log(haiku);
