49c3e51dde2a8867bbbd28458403b1b72d74ccd0
Kennedy DeSousa <kennedydesousa@gmail.com>
2017-11-09 06:16:30 -0800

    changing loop quiz complete

---
+++ Elective-Course-Intro-Javascript/Lesson-4-Loops/changing-the-loop-4-4.js
@@ -0,0 +1,13 @@
+// Directions:
+// Rewrite the following while loop as a for loop:
+//
+// var x = 9;
+// while (x >= 1) {
+//   console.log("hello " + x);
+//   x = x - 1;
+// }
+// Your Code:
+
+for(var x = 9; x >= 1; x -= 1){
+  console.log("hello " + x);
+}
