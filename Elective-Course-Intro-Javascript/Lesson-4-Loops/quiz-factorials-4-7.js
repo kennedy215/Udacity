a410f05ce7c03f59e353d8ecb342ad540cbfffb3
Kennedy DeSousa <kennedydesousa@gmail.com>
2017-11-10 12:59:34 -0800

    factorial quiz complete

---
+++ Elective-Course-Intro-Javascript/Lesson-4-Loops/quiz-factorials-4-7.js
@@ -0,0 +1,15 @@
+// Directions:
+// Write a for loop that prints out the factorial of the number 12:
+//
+// A factorial is calculated by multiplying a number by all the numbers below it. For instance, 3! or "3 factorial" is 3 * 2 * 1 = 6
+//
+// 3!=3∗2∗1=6
+// 4!=4∗3∗2∗1=24
+// 5!=5∗4∗3∗2∗1=120
+// Save your final answer in a variable called solution and print it to the console.
+
+solution = 12;
+for (var fact = 1; fact < 12; fact ++) {
+  solution *= fact;
+}
+ console.log(solution);
