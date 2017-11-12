5fa5efe5e800b1490b4d2e798d0e3eed994b3136
Kennedy DeSousa <kennedydesousa@gmail.com>
2017-11-12 13:24:21 -0800

    finished quiz

---
+++ Elective-Course-Intro-Javascript/Lesson-5-Functions/laugh-it-off-5-1.js
@@ -0,0 +1,8 @@
+// Declare a function called laugh() that returns "hahahahahahahahahaha!". Print the value returned from the laugh() function to the console.
+
+function laugh(){
+  var message = "hahahahahahahahahaha!";
+  return message;
+}
+
+console.log(laugh());
